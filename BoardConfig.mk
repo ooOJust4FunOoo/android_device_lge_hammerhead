24a373ea05f87fdb9641a008472f00c087d5a060
Alx31 <alex31.couderc@gmail.com>
2016-01-24 00:12:37 +0100

    added missing append on toolchains
    Change-Id: I9ef999fa4c94c5c002c0d0f149822a723a14be38

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,11 +113,11 @@ TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 4.9
+TARGET_GCC_VERSION_EXP := 4.9-uber
 
 # Optimizations
 CLANG_O3 := true
