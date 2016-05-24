1218f2459ed77b429705cdb7096adf251bc43e9a
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2016-05-24 16:25:46 -0400

    4.9 tcs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,11 +115,11 @@ TARGET_KERNEL_CONFIG := hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.3/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 5.3
+TARGET_GCC_VERSION_EXP := 4.9
 
 # Optimizations
 CLANG_O3 := true
